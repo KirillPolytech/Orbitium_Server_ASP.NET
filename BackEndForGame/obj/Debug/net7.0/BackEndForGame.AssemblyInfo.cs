1e0178948032b4236e86d844ce805aa881ade2df
Kirill <k.d.kuznetsov@stud.mospolytech.ru>
2024-09-10 20:39:45 +0300

    1.3

--- BackEndForGame/obj/Debug/net7.0/BackEndForGame.AssemblyInfo.cs
+++ BackEndForGame/obj/Debug/net7.0/BackEndForGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEndForGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cddba8aff4301b026ded2b69227589abd2a49155")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEndForGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEndForGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
